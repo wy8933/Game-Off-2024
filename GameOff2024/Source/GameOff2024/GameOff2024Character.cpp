3e3f43532aae8df5dd85d87ed4b1bffb6dafae2d
Austen Kinney <AJKKinney@gmail.com>
2024-11-02 16:09:47 +0900

    Disable Jumping

--- GameOff2024/Source/GameOff2024/GameOff2024Character.cpp
+++ GameOff2024/Source/GameOff2024/GameOff2024Character.cpp
@@ -52,8 +52,8 @@ void AGameOff2024Character::SetupPlayerInputComponent(UInputComponent* PlayerInp
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
 	{
 		// Jumping
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		//EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		//EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
 		// Moving
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AGameOff2024Character::Move);
